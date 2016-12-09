7c71876c3575fd89b3da7f0660bbf4fea05b3e8f
Nick Niles <nickyniles@gmail.com>
2016-12-09 18:16:43 +0100

    lots of stuff

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 
 
 
-TweenMax.set('#hand',{x:"-1000px",y:"1000px"});
+TweenMax.set('#hand',{x:"1000px",y:"1000px"});
 
 var tl = new TimelineLite()
 
-$('#circle').on('click', function(){
-	TweenMax.to('#hand',.5,{x:0,y:0,ease:Power4.easeOut})
-	TweenMax.to('svg defs filter fe')
+$('#home').on('click', function(){
+	TweenMax.to('#hand',.5,{x:0,y:0,ease:Power4.easeOut, onComplete: function(){
+
+	TweenMax.set($('.tab:last-child'),{className:"+=hovered"})
+	}})
+});
+
+
+$('.tab').on('click', function(){
+	TweenMax.to('#hand',1,{x:"1000px",y:"1000px",ease:Power4.easeIn})
+	TweenMax.set($('.tab:last-child'),{className:"-=hovered"})
 });
