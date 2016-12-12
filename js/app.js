426b950b601c54a5b67f2d8315219b773c2ef829
Nick Niles <nickyniles@gmail.com>
2016-12-12 16:46:26 +0100

    add both hands

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 
 
 
-TweenMax.set('#hand',{x:"1000px",y:"1000px"});
+TweenMax.set('#point-hand',{x:"1000px",y:"1000px"});
+TweenMax.set('#illustration',{y:"190"});
 
 var tl = new TimelineLite()
 
-$('#home').on('click', function(){
-	TweenMax.to('#hand',.5,{x:0,y:0,ease:Power4.easeOut, onComplete: function(){
+$('#home-btn').on('click', function(){
+	TweenMax.to('#point-hand',.5,{x:0,y:0,ease:Power4.easeOut, repeat: 1, yoyo:true, onComplete: function(){
 
-	TweenMax.set($('.tab:last-child'),{className:"+=hovered"})
+	// TweenMax.set($('.tab:last-child'),{className:"+=hovered"})
 	}})
 });
 
 
 $('.tab').on('click', function(){
-	TweenMax.to('#hand',1,{x:"1000px",y:"1000px",ease:Power4.easeIn})
+	TweenMax.to('#point-hand',1,{x:"1000px",y:"1000px",ease:Power4.easeIn})
 	TweenMax.set($('.tab:last-child'),{className:"-=hovered"})
 });
